a0edbecab0fa2a3e42afc42429f3a46fde4f5fa9
NureniJamiu <nurenijamiu951@gmail.com>
2026-07-15 20:00:02 +0100

    feat: implement cmd_cat_file bridge function

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <vector>
 
+#include "object.learn.hpp"
 #include "repository.hpp"
 #include <CLI/CLI.hpp>
 
@@ -14,10 +15,37 @@ void cmd_init(const std::string &path) {
     std::cout << "Initializing empty Geet repository in: " << fs::absolute(path)
               << "\n";
   } catch (const std::exception &e) {
-    std::cerr << "Fatal: " << e.what()
-              << "\n"; // remember to get an explanation for this later.
+    std::cerr << "Fatal: " << e.what() << "\n";
   }
 }
+
+void cmd_cat_file(const std::string &type, const std::string &object_sha) {
+  try {
+    auto repo = repo_find();
+    if (!repo) {
+      std::cerr << "Fatal: not a geet repositiry\n";
+      return;
+    }
+
+    std::string sha = object_find(*repo, object_sha, type);
+
+    auto obj = object_read(*repo, sha);
+    if (!obj) {
+      std::cerr << "Fatal: object not found: " << sha << "\n";
+      return;
+    }
+
+    std::vector<unint8_t> data = obj->serialize();
+
+    std::cout.write(reinterpret_cast<const char *>(data.data()),
+                    static_cast<std::streamsize>(data.size()));
+
+  } catch (const std::exception &e) {
+    std::cerr << "Fatal: " << e.what() << "\n";
+  }
+}
+
+// hash object
 
 int main(int argc, char **argv) {
   CLI::App app{"Geet: A light, educational Git implementation in C++"};
